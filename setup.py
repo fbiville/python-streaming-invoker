24f03fec678ea765c51baff18a375200f95c59b1
Florent Biville <fbiville@pivotal.io>
2019-09-10 14:44:40 +0300

    Add pypi configuration

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="python-streaming-invoker",
+    version="0.1.0",
+    description="Python Streaming Invoker for riff",
+    license="Apache2",
+    author="Florent Biville",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
